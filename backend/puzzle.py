ff0c617d26bca92362c4b0110bded8bc0acf6b82
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2024-11-04 01:09:14 -0500

    make a puzzle

--- backend/puzzle.py
+++ backend/puzzle.py
@@ -1,21 +1,41 @@
 import random
+from collections import defaultdict
 
 Puzzle = list[list[int]]
 
 
-def create_dominos(grid_size: int) -> Puzzle:
-    return [[i] + ([0] * (grid_size - 2)) + [i] for i in range(grid_size)]
+def create_puzzle(grid_size: int) -> tuple[Puzzle, Puzzle]:
+    puzzle = create_dominos(int(grid_size))
+    shuffle_dominos(puzzle)
+
+    best_puzzle = None
+    best_num_dominos = float("inf")
+    for _ in range(10000):
+        puzzle, num_dominos = dominos_to_puzzle(puzzle)
+        if num_dominos < best_num_dominos:
+            best_num_dominos = num_dominos
+            best_puzzle = puzzle
+
+    empty_puzzle = remove_solution(best_puzzle)
+    return empty_puzzle, best_puzzle
 
 
-def solve_puzzle(puzzle: Puzzle) -> None:
-    # will fill in the puzzle with the solution
-    ...
+def remove_solution(puzzle: Puzzle) -> Puzzle:
+    # need to find the dominos that have num neighbors = 1
+    def get_neighbors(x, y):
+        return [
+            (x + dx, y + dy)
+            for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0))
+            if 0 <= x + dx < len(puzzle) and 0 <= y + dy < len(puzzle)
+        ]
 
+    def get_degree(x, y):
+        return sum(puzzle[x][y] == puzzle[nx][ny] for nx, ny in get_neighbors(x, y))
 
-def is_unique(puzzle: Puzzle) -> bool:
-    # check if the puzzle is unique
-    # basically need to ensure that there is only ONE solution
-    ...
+    return [
+        [puzzle[x][y] if get_degree(x, y) == 1 else 0 for y in range(len(puzzle))]
+        for x in range(len(puzzle))
+    ]
 
 
 def create_dominos(grid_size: int) -> Puzzle:
@@ -69,6 +89,16 @@ def shuffle_dominos(puzzle: Puzzle, iterations=-1) -> None:
 
 
 def dominos_to_puzzle(dominos: Puzzle, connections: int = -1) -> Puzzle:
+    # make bottom right cell connect
+    dominos[-1][-1] = dominos[-1][-2]
+
+    def get_neighbors(x, y):
+        return [
+            (x + dx, y + dy)
+            for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0))
+            if 0 <= x + dx < len(dominos) and 0 <= y + dy < len(dominos)
+        ]
+
     if connections < 0:
         connections = len(dominos) ** 2
         connections = random.randint(connections, connections * 2)
@@ -83,17 +113,31 @@ def dominos_to_puzzle(dominos: Puzzle, connections: int = -1) -> Puzzle:
         return parents[x]
 
     def union(x, y):
-        parents[find(x)] = find(y)
+        x = find(x)
+        y = find(y)
+        parents[x] = y
 
     def get_degree(neighbors, value):
         return sum(find(dominos[x][y]) == value for x, y in neighbors)
 
-    def get_neighbors(x, y):
-        return [
-            (x + dx, y + dy)
-            for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0))
-            if 0 <= x + dx < len(dominos) and 0 <= y + dy < len(dominos)
-        ]
+    def is_cycle(x, y, allowed_values):
+        color = defaultdict(int)
+
+        def dfs(x, y, parent):
+            if color[(x, y)] == 1:
+                return True
+            color[(x, y)] = 1
+            for nx, ny in get_neighbors(x, y):
+                if (nx, ny) == parent:
+                    continue
+                if find(dominos[nx][ny]) not in allowed_values:
+                    continue
+                if dfs(nx, ny, (x, y)):
+                    return True
+            color[(x, y)] = 2
+            return False
+
+        return dfs(x, y, None)
 
     for _ in range(connections):
         # randomly connect the dominos
@@ -107,12 +151,24 @@ def dominos_to_puzzle(dominos: Puzzle, connections: int = -1) -> Puzzle:
             # shuffle the neighbors so we can connect to a random domino
             random.shuffle(neighbors)
             for nx, ny in neighbors:
+                # edge node
                 if get_degree(get_neighbors(nx, ny), find(dominos[nx][ny])) < 2:
+                    # need to also check that this doesn't create a cycle, how do we check?
+                    # dfs search
+                    if is_cycle(x, y, (find(dominos[x][y]), find(dominos[nx][ny]))):
+                        continue
+                    # we can just make keep track of all neighboring nodes of ALL nodes in the current parent
                     union(dominos[x][y], dominos[nx][ny])
                     break
-    remaining_parents = [i for i in range(max_num + 1) if find(i) == i]
+
+    # reindex the dominos
+    remaining_parents = [i for i in range(1, max_num + 1) if find(i) == i]
     parent_to_reindex = {parent: i + 1 for i, parent in enumerate(remaining_parents)}
-    return [[parent_to_reindex[find(d)] for d in row] for row in dominos]
+
+    # TODO: need to check the final bottom right cell
+    return [[parent_to_reindex[find(d)] for d in row] for row in dominos], len(
+        remaining_parents
+    )
 
 
 def print_puzzle(puzzle: Puzzle):
@@ -144,5 +200,20 @@ if __name__ == "__main__":
     print_puzzle(puzzle)
 
     print("\nFinal puzzle:")
-    puzzle = dominos_to_puzzle(puzzle)
+    best_puzzle = None
+    best_num_dominos = float("inf")
+    for _ in range(10000):
+        puzzle, num_dominos = dominos_to_puzzle(puzzle)
+        if num_dominos < best_num_dominos:
+            best_num_dominos = num_dominos
+            best_puzzle = puzzle
+
     print_puzzle(puzzle)
+
+    puzzle, solution = create_puzzle(grid_size)
+
+    print("\nEmpty puzzle:")
+    print_puzzle(puzzle)
+
+    print("\nSolution:")
+    print_puzzle(solution)
